cbac36a43cd519eb367fec67c54d803ece5415f8
Andrii Tykhonov <tianodessa@gmail.com>
2021-12-26 20:42:04 +0200

    Test commit 5

--- TesModule/src/workSpace/ClazzTest.java
+++ TesModule/src/workSpace/ClazzTest.java
@@ -4,5 +4,6 @@ public class ClazzTest {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("one more Hi");
+        System.out.println("....");
     }
 }
