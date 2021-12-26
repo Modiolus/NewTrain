5e9594909621cdbcb29fcf9b93a855b66217eb0e
Andrii Tykhonov <tianodessa@gmail.com>
2021-12-26 22:26:21 +0200

    Commit10 modif

--- TesModule/src/workSpace/Clazx.java
+++ TesModule/src/workSpace/Clazx.java
@@ -1,4 +1,8 @@
 package workSpace;
 
 public class Clazx {
+    public static void main(String[] args) {
+        System.out.println("Main");
+        System.out.println("Second main out");
+    }
 }
